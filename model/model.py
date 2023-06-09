cd38ebcc7d69c04e1e9a8163e2b3fa243c9bfa1c
Sid Shanker <sid.shanker@baseten.co>
2023-06-09 11:03:50 -0700

    Get falcon-7b in better shape.

--- model/model.py
+++ model/model.py
@@ -1,25 +1,26 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-import transformers
-from typing import Dict, List
+from transformers import AutoTokenizer, pipeline
+from typing import Dict
 
-CHECKPOINT = "tiiuae/falcon-40b"
+CHECKPOINT = "tiiuae/falcon-7b-instruct"
 DEFAULT_MAX_LENGTH = 128
 DEFAULT_TOP_P = 0.95
 
+
 class Model:
-    def __init__(self, data_dir: str, config: Dict, secrets: Dict, **kwargs) -> None:
+    def __init__(self, data_dir: str, config: Dict, **kwargs) -> None:
         self._data_dir = data_dir
         self._config = config
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.tokenizer = None
         self.pipeline = None
 
     def load(self):
-        tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
-        self.pipeline = transformers.pipeline(
+        self.tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
+        self.pipeline = pipeline(
             "text-generation",
             model=CHECKPOINT,
-            tokenizer=tokenizer,
+            tokenizer=self.tokenizer,
             torch_dtype=torch.bfloat16,
             trust_remote_code=True,
             device_map="auto",
@@ -30,24 +31,12 @@ class Model:
         with torch.no_grad():
             try:
                 prompt = request.pop("prompt")
-                max_length = request.pop("max_length", DEFAULT_MAX_LENGTH)
-                top_p = request.pop("top_p", DEFAULT_TOP_P)
-                encoded_prompt = self._tokenizer(prompt, return_tensors="pt").input_ids
-
-                encoded_output = self._model.generate(
-                    encoded_prompt,
-                    max_length=max_length,
-                    top_p=top_p,
+                data = self.pipeline(
+                    prompt,
+                    eos_token_id=self.tokenizer.eos_token_id,
                     **request
                 )[0]
-                decoded_output = self._tokenizer.decode(
-                    encoded_output, skip_special_tokens=True
-                )
-                instance_response = {
-                    "completion": decoded_output,
-                    "prompt": prompt,
-                }
+                return {"data": data}
 
-                return {"status": "success", "data": instance_response, "message": None}
             except Exception as exc:
                 return {"status": "error", "data": None, "message": str(exc)}
